3ec3ef4c92f15e2da0b15254f6813308c24805b5
Ryoda <73408489+Ryodaa@users.noreply.github.com>
2021-12-06 20:27:07 +0100

    removed useless junk

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var blink_speed = 1000; // every 1000 == 1 second, adjust to suit
 var t = setInterval(function () {
-    var ele = document.getElementById('myBlinkingDiv');
+    var ele = document.getElementById('blinkingDiv');
     ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
 }, blink_speed);
