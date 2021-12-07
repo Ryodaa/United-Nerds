cb2883c26bb60880fcd696680dd5fcc04cdb17ff
Ryoda <73408489+Ryodaa@users.noreply.github.com>
2021-12-07 15:23:36 +0100

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var blink_speed = 1000; // every 1000 == 1 second, adjust to suit
+var blink_speed = 800; // every 1000 == 1 second, adjust to suit
 var t = setInterval(function () {
     var ele = document.getElementById('blinkingDiv');
     ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
