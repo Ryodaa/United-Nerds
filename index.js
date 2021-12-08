98b99aae443013c5c15a383b0899669249ce30bc
Ryoda <73408489+Ryodaa@users.noreply.github.com>
2021-12-08 08:54:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ setInterval(function () {
     var ele = document.getElementById('blinkingDiv');
     ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
 }, blink_speed);
+
+var test = 100;
+setInterval(function () {
+    var element = document.getElementById("title");
+    element.style.transform = (element.style.transform == 'rotate(5deg)' ? '' : 'rotate(5deg)');
+}, test);
